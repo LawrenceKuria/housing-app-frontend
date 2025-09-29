5c8808ccfce853f00b6248015bfbde8431f01192
Lawrence Kuria <i3uyer2@gmail.com>
2025-09-28 23:02:50 -0700

    Configured shadcn setup

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-    <h1 className="text-lg font-semibold">Hello App!</h1>
-    </>
-  )
-}
-
-export default App
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,31 @@
 import * as React from 'react'
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { Outlet, createRootRoute, Link } from '@tanstack/react-router'
+import Header from '@/components/ui/Header'
 
 export const Route = createRootRoute({
   component: RootComponent,
+  notFoundComponent: NotFound,
 })
 
 function RootComponent() {
   return (
     <React.Fragment>
-      <div>Hello "__root"!</div>
+      <Header />
       <Outlet />
     </React.Fragment>
+  )
+}
+
+function NotFound () {
+  return (
+    <div className="flex flex-col items-center justify-center text-center py-20">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        404
+      </h1>
+      <p className='text-lg text-gray-600 mb-6'>
+        Oops, the page you are looking for doesn't exist.
+      </p>
+      <Link to='/' className='px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition' >Go Back Home</Link>
+    </div>
   )
 }
