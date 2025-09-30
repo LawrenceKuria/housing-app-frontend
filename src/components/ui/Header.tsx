059a8afae1eec3403a316a63e06c5a73c3923877
Lawrence Kuria <i3uyer2@gmail.com>
2025-09-29 20:48:09 -0700

    Added header and other utilites

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,31 +1,68 @@
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu"
+import { Link } from "@tanstack/react-router";
 
 const Header = () => {
-    return ( 
-    <NavigationMenu>
-        <NavigationMenuList>
-            <NavigationMenuItem>
-                <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                    <NavigationMenuLink>Link</NavigationMenuLink>
-                </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-                <NavigationMenuTrigger>Create Post</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                    <NavigationMenuLink>Link</NavigationMenuLink>
-                </NavigationMenuContent>
-            </NavigationMenuItem>
-        </NavigationMenuList>
-    </NavigationMenu>
-    );
+    const user = {
+        name: "Lawrence"
+    }
+
+    const handleLogout = () => {
+        console.log('User logged out');
+    }
+
+    return (
+    <header className="bg-gray-100 shadow-sm">
+      <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+        
+        <Link to="/" className="flex items-center">
+          <span className="ml-2 text-2xl font-bold text-black">SlugFind</span>
+        </Link>
+
+        <nav className="flex items-center space-x-4">
+          <Link
+            to="/"
+            className="text-gray-700 hover:text-black font-medium transition-colors"
+          >
+            Listings
+          </Link>
+          { user && (
+            <Link
+              to="/"
+              className="
+                inline-block bg-blue-600 hover:bg-blue-700 text-white font-medium
+                px-4 py-2 rounded-lg shadow transition"
+            >
+              + New Listing
+            </Link>
+          )}
+          
+        </nav>
+        <div className="flex items-center space-x-2">
+          {
+            !user ? (
+              <>
+                <Link to="/" className="text-gray-600 hover:text-gray-700 font-medium transition px-3 py-2 leading-none">
+                  Login
+                </Link>
+                <Link to="/" className="bg-gray-100 hover:bg-gray-200 text-gray-800 font-medium transition px-4 py-2 rounded-md leading-none">
+                  Register
+                </Link>
+              </>
+            ) : (
+              <>
+                <span className="text-gray-700 font-medium px-2 hidden sm:block">
+                  Welcome, {user.name}
+                </span>
+                <button onClick={handleLogout} className="text-red-600 hover:text-red-900 font-medium transition px-3 py-2 leading-none cursor-pointer">
+                  Logout
+                </button>
+              </>
+            )
+          }
+          
+        </div>
+      </div>
+    </header>
+  )
 }
  
 export default Header;
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export type Post = {
+    id: string
+    title: string
+    description: string
+    tags: string[]
+    createdAt: string
+    user: string
+}
